05e009e25d5ae3110d0be5d9d64249718743aa64
NoaTakeshi <pipepaipei2014@gmail.com>
2026-05-05 19:52:03 -0500

    render backend

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbo: {
-    resolveAlias: {
-      'inferno': 'inferno/dist/index.dev.esm.js',
-    },
-  },
+  // Eliminamos 'turbo' porque estaba causando el Warning en tu consola
   async rewrites() {
+    // Si la variable BACKEND_URL existe (Producción), usa esa. 
+    // Si no, usa el puerto 8000 de tu PC (Desarrollo local).
+    const backendUrl = process.env.BACKEND_URL || 'http://127.0.0.1:8000';
+    
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/v1/:path*',
+        destination: `${backendUrl}/api/v1/:path*`,
       },
     ];
   },
